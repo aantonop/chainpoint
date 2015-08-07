d3dedb9055ad342d36ceabbcfd7da6e0ce80df6b
Shawn Wilkinson <shawn@storj.io>
2015-08-07 19:52:28 -0400

    Update Version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     #  Uncomment one or more lines below in the install_requires section
     #  for the specific client drivers/modules your application needs.
-    install_requires=['Flask == 0.10.1', 'Flask-SQLAlchemy == 2.0', 'btctxstore == 3.0.0'],
+    install_requires=['Flask == 0.10.1', 'Flask-SQLAlchemy == 2.0', 'btctxstore == 4.0.0'],
     tests_require=['coverage', 'coveralls'],
     test_suite="tests",
 )
